71afa652a6d42446024f4ca3ea2cd53e2867da7e
paragparalikar <paragparalikar@gmail.com>
2025-04-19 10:39:30 +0530

    na

--- src/main/java/com/bicycle/backtest/workflow/stage/HyperParameterOptimizationStage.java
+++ src/main/java/com/bicycle/backtest/workflow/stage/HyperParameterOptimizationStage.java
@@ -7,6 +7,7 @@ import smile.data.DataFrame;
 import smile.data.formula.Formula;
 import smile.util.Index;
 import smile.validation.Bag;
+import smile.validation.ClassificationMetrics;
 import smile.validation.CrossValidation;
 
 import java.util.List;
@@ -29,7 +30,7 @@ public class HyperParameterOptimizationStage {
                     for(int nodeSize = 10; nodeSize <= 50; nodeSize += 10){
                         for(int ntrees = 50; ntrees <= 1000; ntrees += 50){
                             final RandomForest.Options options = new RandomForest.Options(ntrees, mtry, maxDepth, 0, nodeSize);
-                            final double score = compute(folds, formula, dataFrame, options, evaluator);
+                            final double score = compute(folds, formula, dataFrame, options, evaluator, false);
                             System.out.printf("%10d%10d%10d%10d%10.4f\n", ntrees, mtry, maxDepth, nodeSize, score);
                             featureWriter.writeValues(List.of((float)ntrees, (float)mtry, (float)maxDepth, (float)nodeSize, (float)score));
                             if(score > maxScore){
@@ -44,22 +45,36 @@ public class HyperParameterOptimizationStage {
         System.out.printf("Max score : %f\n", maxScore);
         System.out.printf("RandomForest.Options : ntrees=%d, mtry=%d, maxDepth=%d, nodeSize=%d\n",
                 maxOptions.ntrees(), maxOptions.mtry(), maxOptions.maxDepth(), maxOptions.nodeSize());
+        compute(folds, formula, dataFrame, maxOptions, evaluator, true);
         return maxOptions;
     }
 
-    private double compute(int k, Formula formula, DataFrame dataFrame, RandomForest.Options options, BiFunction<int[], int[], Double> evaluator){
+    private double compute(int k, Formula formula, DataFrame dataFrame, RandomForest.Options options, BiFunction<int[], int[], Double> evaluator, boolean printInfo){
         double sum = 0;
         final Bag[] bags = CrossValidation.of(dataFrame.size(), k);
+        if(printInfo) System.out.printf("%15s%15s%15s%15s%15s%15s%15s%15s%15s%15s%15s%15s%15s\n", "train", "test", "score", "error", "accuracy", "sensitivity",
+                "specificity", "precision", "f1", "mcc", "auc", "logLoss", "crossEntropy");
         for(int bagIndex = 0; bagIndex < k; bagIndex++){
             final Bag bag = bags[bagIndex];
             final DataFrame train = dataFrame.get(Index.of(bag.samples()));
             final RandomForest model = RandomForest.fit(formula, train, options);
+
             final DataFrame test = dataFrame.get(Index.of(bag.oob()));
             final double trainResult = evaluate(formula, train, model, evaluator);
             final double testResult = evaluate(formula, test, model, evaluator);
-            sum += ((trainResult + testResult) / Math.pow(1 + Math.abs(trainResult - testResult), 2));
+            final double score = ((trainResult + testResult) / Math.pow(1 + Math.abs(trainResult - testResult), 2));
+            sum += score;
+            if(printInfo) printInfo(model, trainResult, testResult, score);
         }
         return sum / k;
+    }
+
+    private void printInfo(RandomForest model, double trainResult, double testResult, double score){
+        final ClassificationMetrics metrics = model.metrics();
+        System.out.printf("%15.2f%15.2f%15.2f%15d%15.2f%15.2f%15.2f%15.2f%15.2f%15.2f%15.2f%15.2f%15.2f\n",
+                trainResult, testResult, score, metrics.error(), metrics.accuracy(),
+                metrics.sensitivity(), metrics.specificity(), metrics.precision(), metrics.f1(), metrics.mcc(), metrics.auc(),
+                metrics.logloss(), metrics.crossEntropy());
     }
 
     private double evaluate(Formula formula, DataFrame dataFrame, RandomForest model, BiFunction<int[], int[], Double> evaluator){
